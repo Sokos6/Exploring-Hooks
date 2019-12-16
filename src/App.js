f02e95d9ecc14fba092877951f423314c199e70b
Sokos6 <will.sokolowski@gmail.com>
2019-12-15 19:00:28 -0600

    render props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
 import Button from './Button';
+import DataLoader from './DataLoader';
 
 function App() {
   return (
     <div className="App">
       <Button />
+      <DataLoader
+        render={data => {
+          return (
+            <div>
+              <ul>
+                {data.map(el => (
+                  <li key={el.id}>{el.title}</li>
+                ))}
+              </ul>
+            </div>
+          );
+        }}
+      />
     </div>
   );
 }
---
+++ src/DataLoader.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export default class DataLoader extends Component {
+  state = { data: [] };
+
+  componentDidMount() {
+    fetch("http://localhost:3001/links")
+      .then(response => response.jason())
+      .then(data =>
+        this.setState(() => {
+          return { data };
+        })
+      );
+  }
+
+  render() {
+    return this.props.render(this.state.data);
+  }
+}
