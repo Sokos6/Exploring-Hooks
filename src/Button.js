d9a3f074ab5e235dccb37594afb974d9798edb8f
Sokos6 <will.sokolowski@gmail.com>
2019-12-15 18:38:50 -0600

    use state

--- src/Button.js
+++ src/Button.js
@@ -1,21 +1,11 @@
-import React, { Component } from 'react'
+import React, { useState } from "react";
 
-export default class Button extends Component {
-  constructor() {
-    super();
-    this.state = { buttonText: "Click Me" };
-    this.handleClick = this.handleClick.bind(this);
-  }
+export default function Button() {
+  const [buttonText, setButtonText] = useState("Click Me");
 
-  handleClick() {
-    this.setState(() => {
-      return { buttonText: "Thanks!" };
-    });
-  }
-
-  render() {
-    const { buttonText } = this.state;
-    return <button onClick={this.handleClick}>{buttonText}</button>;
-  }
-
+  return (
+    <button onClick={() => setButtonText("Thanks")}>
+      {buttonText}
+    </button>
+  );
 }
